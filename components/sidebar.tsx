c72f1de9e4a94f2c651545c257ba9d01d1629425
simonbeirouti <hello@simonbeirouti.com>
2022-09-20 20:43:50 +1000

    attempt fix key issues on component

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -98,7 +98,7 @@ export default function sidebar() {
                         <PlaylistItem 
                             name={playlist}
                             route="/"
-                        ></PlaylistItem>
+                        />
                     ))}
                 </List>
             </Box>
