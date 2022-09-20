aa6fab5eadb480092defd8b54abb1653ae8d2d1e
simonbeirouti <hello@simonbeirouti.com>
2022-09-20 17:54:09 +1000

    create initial sidebar

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -1,10 +1,18 @@
 import { Box } from '@chakra-ui/layout'
+import Sidebar from './sidebar'
 
 export default function playerLayout({children}) {
   return (
-    <Box>
-        Layout 
-        {children}
+    <Box width="100vw" height="100vh">
+        <Box position="absolute" top="0" width="250px">
+            <Sidebar></Sidebar>
+        </Box>
+        <Box marginLeft="250px" margin="100">
+            {children}
+        </Box>
+        <Box position="absolute" left="0" bottom="0">
+            Bottom box
+        </Box>
     </Box>
   )
 }
