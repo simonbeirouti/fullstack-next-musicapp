a8d3524941db9bd4e8b5a6954c1e43565e02f67a
simonbeirouti <hello@simonbeirouti.com>
2022-09-20 18:15:18 +1000

    fix styling to move to correct position

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -4,11 +4,13 @@ import Sidebar from './sidebar'
 export default function playerLayout({children}) {
   return (
     <Box width="100vw" height="100vh">
-        <Box position="absolute" top="0" width="250px">
+        <Box position="absolute" top="0" width="250px" left="0">
             <Sidebar></Sidebar>
         </Box>
-        <Box marginLeft="250px" margin="100">
+        <Box marginLeft="250px" marginBottom="100px">
+          <Box height="calc(100vh - 100px)">
             {children}
+          </Box>
         </Box>
         <Box position="absolute" left="0" bottom="0">
             Bottom box
