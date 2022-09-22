7ef248c792a0dfed65fa6a6e9514ddfd5c1bc830
simonbeirouti <hello@simonbeirouti.com>
2022-09-22 21:14:57 +1000

    create signin and signup pages

---
+++ pages/signup.tsx
@@ -0,0 +1,11 @@
+import AuthForm from "../components/authForm"
+
+const Signup = () => {
+    return (
+        <AuthForm mode="Signup" />
+    )
+}
+
+Signup.authpage = true
+
+export default Signup
