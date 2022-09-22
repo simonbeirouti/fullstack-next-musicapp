32a86a07564b40a1a4fc7c3261f793e0ed6b11b5
simonbeirouti <hello@simonbeirouti.com>
2022-09-22 21:14:47 +1000

    add if else to check login to show menu

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,9 +33,14 @@ const theme = extendTheme({
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Layout>
+      {Component.authpage ? (
         <Component {...pageProps} />
+        ): (
+        <Layout>
+          <Component {...pageProps} />
       </Layout>
+      )}
+      
     </ChakraProvider>
   )
 }
